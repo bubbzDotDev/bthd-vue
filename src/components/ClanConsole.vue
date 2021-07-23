f684c42eb7d5c8e0f7fe370bc03884388b6597fe
bubbzDotDev <christopher.blount@outlook.com>
2021-07-23 14:08:59 -0500

    got the welcome to dashboard message working finally

--- src/components/ClanConsole.vue
+++ src/components/ClanConsole.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <p v-for="user in filteredUsers" :key="user.id">
-      Welcome to the Dashboard, {{ user.bungieName }}
-    </p>
+  <div class="console">
+    <h2>Welcome to the dashboard, {{ user.bungieName }}!</h2>
   </div>
 </template>
 
@@ -13,46 +11,47 @@ import { ref, computed } from 'vue'
 
 export default {
   setup() {
-    const filteredUsers = ref([]);
+    const user = ref({});
     let usersTarget;
     const store = useStore();
     const usersDb = new UsersDb();
-    // users.value = await usersDb.setUsers();
+    
+    const authUser = computed(() => { // Use ".uid" on this to get the ID
+      return store.getters['auth/user']; 
+    });
+
     usersDb.setUsers();
 
-    // const authUser = computed(() => { // Use ".uid" on this to get the ID
-    //   return store.getters['auth/user']; 
-    // });
-
-    setTimeout(async () => {
+    setTimeout(() => {
       const users = computed(() => {
         return store.getters['users/users'];
       });
       users.value
         .then(result => {
           usersTarget = Object.assign({}, result);
-          console.log('usersTarget:', usersTarget);
         })
         .then(() => {
-          let objectToArray = Object.entries(usersTarget);
-          filteredUsers.value = objectToArray.filter(([key, value]) => {
-            console.log('key:', key);
-            console.log('value:', value);
-            // if (authUser.value.uid === doc.id) {
-            //   console.log('doc:', doc);
-            //   return doc;
-            // }
-          });
+          for (const property in usersTarget) {
+            if(usersTarget[property].id === authUser.value.uid) {
+              user.value = usersTarget[property];
+            }
+          }
         });
-    }, 1000);
+    }, 300);
     
     return {
-      filteredUsers
+      user
     };
   }
 }
 </script>
 
 <style scoped>
+.console {
+  color: #fff;
+}
 
+h2 {
+  text-align: center;
+}
 </style>
